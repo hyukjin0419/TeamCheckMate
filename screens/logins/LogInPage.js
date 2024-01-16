6ea7e596330c6bdf0bb12e08c50192a549c03670
hyukjin0419 <21900771@handong.ac.kr>
2024-01-16 14:48:30 +0900

    Navigation Foldering completed

---
+++ screens/logins/LogInPage.js
@@ -0,0 +1,85 @@
+import { useNavigation } from "@react-navigation/core";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../firebase";
+import { Alert } from "react-native";
+import styles from "../styles/css";
+
+export default function LogInPage() {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log("User signed in: ", user.email);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  //로그인 관련 함수
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Logged in with: " + user.email);
+      })
+      .catch((error) => Alert.alert(error.message));
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.head}>
+        <TouchableOpacity
+          style={styles.headBtn}
+          onPress={() => navigation.navigate("InitialPage")}
+        >
+          <AntDesign name="left" size={20} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.title}>로그인</Text>
+      </View>
+
+      <View style={styles.textBox}>
+        <TextInput
+          placeholder="이메일"
+          autoCapitalize="none"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.textInput}
+          keyboardType="email-address"
+        />
+        <TextInput
+          placeholder="비밀번호"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.textInput}
+          secureTextEntry
+        />
+      </View>
+      <View>
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={{ ...styles.button, backgroundColor: "#050026" }}
+        >
+          <Text style={{ ...styles.buttonText, color: "white" }}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
---
+++ screens/styles/css.js
@@ -0,0 +1,58 @@
+import { StyleSheet } from "react-native";
+
+const commonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: "5%",
+    backgroundColor: "white",
+  },
+  //헤더
+  head: {
+    marginTop: "18%",
+    flexDirection: "row",
+  },
+  //헤더에 들어가는 타이틀 -> 글꼴수에 따라 marginLeft 수정 바람.
+  title: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginLeft: "36%",
+  },
+  //로그인 화면에서 쓰이는 textBox
+  textBox: {
+    marginTop: "10%",
+  },
+  //로그인 화면에서 쓰이는 textInput
+  textInput: {
+    margin: "3%",
+    fontSize: 16,
+    borderBottomColor: "grey",
+    borderBottomWidth: 2,
+    paddingBottom: 10,
+  },
+  //버튼 기본 디자인
+  button: {
+    borderWidth: 1,
+    borderRadius: 9,
+    alignItems: "center",
+    paddingVertical: "5%",
+    marginBottom: "3%",
+  },
+  //버튼안 text 기본 디자인
+  buttonText: {
+    fontSize: 15,
+    fontWeight: 400,
+    color: "white",
+  },
+  //가입하기 버튼 & 건너뛰기 버튼처럼 메인 버튼 밑에 있는 작은 글꼴 디자인
+  subButton: {
+    borderBottomWidth: 1,
+    padding: 4,
+    alignSelf: "center",
+  },
+  subButtonText: {
+    textAlign: "center",
+    fontSize: 14,
+  },
+});
+
+export default commonStyles;
