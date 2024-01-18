b572a63d94cc5450d284a85027677305b9aaa1b9
hyukjin0419 <21900771@handong.ac.kr>
2024-01-18 11:45:34 +0900

    code refactoring in SignInPage

--- screens/logins/SignInPage.js
+++ screens/logins/SignInPage.js
@@ -9,7 +9,10 @@ import {
   KeyboardAvoidingView,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
-import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
 import { auth } from "../../firebase.js";
 import { Alert } from "react-native";
 import styles from "../styles/css.js";
@@ -24,81 +27,85 @@ export default function SignInPage() {
   const [studentNumber, setstudentNumber] = useState("");
   const [phoneNumber, setphoneNumber] = useState("");
   const appState = useRef(AppState.currentState);
+  //   active: 앱이 현재 활성화 되어 있습니다.
+  //   background: 앱이 백그라운드에 있습니다.
+  //   inactive: 앱이 비활성화 되었지만 여전히 실행 중입니다.
   const [disableBtn, setDisableBtn] = useState(true);
   const [signInButtonColor, setSignInButtonColor] = useState("#D9D9D9");
   const [appStateVisible, setAppStateVisible] = useState(appState.current);
 
   useEffect(() => {
-    const unsubscribeAuthStateChanged = auth.onAuthStateChanged(async (user) => {
-      if (user) {
-        await user.reload();
-        if (user.emailVerified) {
-          console.log("User signed in:", user.email);
-        }
-      }
-    });
-  
-    const handleAppStateChange = async (nextAppState) => {
-      if (
-        appState.current.match(/inactive|background/) &&
-        nextAppState === "active"
-      ) {
-        const user = auth.currentUser;
+    const unsubscribeAuthStateChanged = auth.onAuthStateChanged(
+      async (user) => {
         if (user) {
           await user.reload();
           if (user.emailVerified) {
-            console.log("User signed in:", user.email);
-            alert("You have successfully signed in");
+            console.log("SignInPage -> User signed in:", user.email);
           }
         }
       }
-  
+    );
+    //handleAppStateChange는 AppState 모듈에서 제공하는 이벤트 핸들러이다. 이 핸들러는 앱의 상태가 변할 때 자동으로 호출된다. 또한 매개변수의 값은 항상 새로운 앱 상태인 nextAppState의 값
+    const handleAppStateChange = async (nextAppState) => {
+      const isInBackground = appState.current.match(/inactive|background/);
+      //만약 백그라운드에 있다가 앱을 active시키면
+      if (isInBackground && nextAppState === "active") {
+        //유저 정보를 받아와서
+        const user = auth.currentUser;
+        //유저 정보를 업데이트 하고
+        if (user) {
+          await user.reload();
+          //만약 이메일이 verified 되었으면
+          if (user.emailVerified) {
+            console.log("User signed in:", user.email);
+            //alert("You have successfully signed in");
+          }
+        }
+      }
+      //여기 무슨뜻인지..?
       appState.current = nextAppState;
       setAppStateVisible(appState.current);
-      console.log("AppState", appState.current);
+      console.log("AppState: " + appState.current);
     };
-  
+
     AppState.addEventListener("change", handleAppStateChange);
-  
+
     return () => {
       unsubscribeAuthStateChanged();
-  
+
       // Check if AppState.removeEventListener is defined before calling
       if (AppState.removeEventListener) {
         AppState.removeEventListener("change", handleAppStateChange);
       }
     };
   }, [navigation]);
-  
 
   //회원가입 관련 함수
   const handleSignUp = () => {
     try {
-      createUserWithEmailAndPassword(auth, email, password)
-      .then(async (userCredentials) => {
-        const user = userCredentials.user;
-        await sendEmailVerification(user)
-        .then(async () => {
-          alert('Verification email sent.');
-          console.log("Registered with: " + user.email);
-          try {
-            const timestamp = new Date();
-            const docRef = await setDoc(doc(db, "user", email), {
-              email: email,
-              name: name,
-              school: school,
-              studentNumber: studentNumber,
-              phoneNumber: phoneNumber,
-              timestamp: timestamp,
-            });
-          } catch (e) {
-            console.error("Error adding document: ", e);
-          }
-        })
-        
-      })
-    }
-    catch (error) {
+      createUserWithEmailAndPassword(auth, email, password).then(
+        async (userCredentials) => {
+          const user = userCredentials.user;
+          await sendEmailVerification(user).then(async () => {
+            alert("Verification email sent.");
+            console.log("Registered with: " + user.email);
+            try {
+              const timestamp = new Date();
+              const docRef = await setDoc(doc(db, "user", email), {
+                email: email,
+                name: name,
+                school: school,
+                studentNumber: studentNumber,
+                phoneNumber: phoneNumber,
+                timestamp: timestamp,
+              });
+            } catch (e) {
+              console.error("Error adding document: ", e);
+            }
+          });
+        }
+      );
+    } catch (error) {
       if (error.code === "auth/email-already-in-use") {
         alert("This account already exists");
       } else {
@@ -106,31 +113,20 @@ export default function SignInPage() {
       }
     }
   };
+  //이메일 비밀번호 입력 되었는지 확인하는 함수
+  useEffect(() => {
+    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    const isPasswordValid = password.length >= 6;
 
-  const emailRequired = (text) => {
-    setEmail(text);
-    if(!email.trim() || !password.trim()) {
-      setDisableBtn(true);
+    setSignInButtonColor(!(isEmailValid && isPasswordValid));
+    if (isEmailValid && isPasswordValid) {
+      setSignInButtonColor(true);
+      setSignInButtonColor("#050026");
+    } else {
+      setSignInButtonColor(false);
       setSignInButtonColor("#D9D9D9");
     }
-    else {
-      setDisableBtn(false);
-      setSignInButtonColor("#050026");
-    }
-  }
-
-  const passwordRequired = (text) => {
-    setPassword(text);
-    if(!email.trim() || !password.trim()) {
-      setDisableBtn(true);
-      setSignInButtonColor("#D9D9D9");
-    }
-    else {
-      setDisableBtn(false);
-      setSignInButtonColor("#050026");
-    }
-  }
-
+  }, [email, password]);
   return (
     <KeyboardAvoidingView style={styles.container}>
       {/*head 부분*/}
@@ -147,17 +143,16 @@ export default function SignInPage() {
         <TextInput
           placeholder="이메일"
           value={email}
-          onChangeText={(text) => emailRequired(text)}
+          onChangeText={(text) => setEmail(text)}
           style={styles.textInput}
           keyboardType="email-address"
           autoCapitalize="none"
-          
         />
         {/*이메일 입력창*/}
         <TextInput
           placeholder="비밀번호"
           value={password}
-          onChangeText={(text) => passwordRequired(text)}
+          onChangeText={(text) => setPassword(text)}
           style={styles.textInput}
           secureTextEntry
         />
@@ -196,7 +191,11 @@ export default function SignInPage() {
         <TouchableOpacity
           onPress={handleSignUp}
           disabled={disableBtn}
-          style={{ ...styles.button, backgroundColor: signInButtonColor, marginTop: 20 }}
+          style={{
+            ...styles.button,
+            backgroundColor: signInButtonColor,
+            marginTop: 20,
+          }}
         >
           <Text style={{ ...styles.buttonText, color: "white" }}>가입하기</Text>
         </TouchableOpacity>
