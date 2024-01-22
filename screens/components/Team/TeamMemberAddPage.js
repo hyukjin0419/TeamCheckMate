97a7a19987f54d58dce3fabd570b84e32ce6319d
hyukjin0419 <21900771@handong.ac.kr>
2024-01-22 15:33:05 +0900

    TeamMemberAddPage css third version completed

--- screens/components/Team/TeamMemberAddPage.js
+++ screens/components/Team/TeamMemberAddPage.js
@@ -11,6 +11,9 @@ import {
   FlatList,
   Image,
   Pressable,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 import { AntDesign, FontAwesome5 } from "@expo/vector-icons";
 import styles from "../../styles/css";
@@ -33,9 +36,7 @@ export default function AddMembers() {
   const navigation = useNavigation();
   //검색중? or Not
   const [isSearching, setIsSearching] = useState(false);
-  const changeSearchingState = () => {
-    setIsSearching(!setIsSearching);
-  };
+
   /*
   1. 파이어 베이스에서 이메일 가져와서 저장하기
   2. 검색창 및 사용자 목록화면 구현
@@ -97,82 +98,122 @@ export default function AddMembers() {
       });
       console.log("이메일 추가 성공");
     } else {
+      Alert.alert("이미 추가된 이메일입니다.");
       console.log("이미 추가된 이메일입니다.");
     }
     console.log("현재 추가된: ", addedUserEmailArray);
   };
-  return (
-    <KeyboardAvoidingView style={s.container}>
-      {/* 헤더부분 */}
-      <View style={s.head}>
-        <TouchableOpacity
-          style={styles.headBtn}
-          onPress={() => navigation.goBack()}
-        >
-          <AntDesign name="left" size={20} color="black" />
-        </TouchableOpacity>
-        <Text style={s.title}>팀 메이트 초대</Text>
-        <TouchableOpacity>
-          <Text style={{ ...s.titleSend }}>보내기</Text>
-        </TouchableOpacity>
-      </View>
 
-      {/* 검색창 */}
-      <View style={s.searchContainer}>
-        <Image
-          style={s.glass}
-          source={require("../../images/icons/glass_grey.png")}
-        />
-        <TextInput
-          value={searchEmail}
-          onChangeText={(text) => {
-            setSearchEmail(text);
-            setIsSearching(text.trim() !== "");
-          }}
-          placeholder="초대할 팀 메이트의 이메일을 입력해주세요!"
-          style={s.textInput}
-          keyboardType="email-address"
-          autoCapitalize="none"
-          returnKeyType="next"
-        />
-      </View>
-      {isSearching ? (
-        /* 검색된 emailContainer */
-        filteredResults.length > 0 && (
-          <View style={s.emailContainer}>
-            <FlatList
-              data={filteredResults}
-              keyExtractor={(item) => item.id.toString()}
-              renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => addEmail(item.id)}>
-                  <Text style={s.emailText}>{item.id}</Text>
-                </TouchableOpacity>
-              )}
-            />
-          </View>
-        )
-      ) : (
-        /* 건너뛰기 버튼 */
-        <View>
-          <Pressable>
-            <View style={s.subBtn}>
-              <Text style={s.subBtnText}>건너뛰기</Text>
-            </View>
-          </Pressable>
-          <View style={s.emailContainer}>
-            <FlatList
-              data={addedUserEmailArray}
-              keyExtractor={(item) => item.id.toString()}
-              renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => addEmail(item.id)}>
-                  <Text style={s.emailText}>{item.id}</Text>
-                </TouchableOpacity>
-              )}
-            />
-          </View>
+  const removeEmail = (emailId) => {
+    // 해당 이메일을 addedUserEmailArray에서 제거
+    const updatedArray = addedUserEmailArray.filter(
+      (item) => item.id !== emailId
+    );
+    setAddedUserEmailArray(updatedArray);
+    console.log("이메일 제거 성공");
+  };
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+        setIsSearching(false);
+      }}
+    >
+      <KeyboardAvoidingView style={s.container}>
+        {/* 헤더부분 */}
+        <View style={s.head}>
+          <TouchableOpacity
+            style={styles.headBtn}
+            onPress={() => navigation.goBack()}
+          >
+            <AntDesign name="left" size={20} color="black" />
+          </TouchableOpacity>
+          <Text style={s.title}>팀 메이트 초대</Text>
+          <TouchableOpacity>
+            <Text style={{ ...s.titleSend }}>보내기</Text>
+          </TouchableOpacity>
         </View>
-      )}
-    </KeyboardAvoidingView>
+
+        {/* 검색창 */}
+
+        <View>
+          <View style={s.searchContainer}>
+            <Image
+              style={s.glass}
+              source={require("../../images/icons/glass_grey.png")}
+            />
+            <TextInput
+              value={searchEmail}
+              onChangeText={(text) => {
+                setSearchEmail(text);
+                setIsSearching(text.trim() !== "");
+              }}
+              onSubmitEditing={() => {
+                setSearchEmail("");
+                setIsSearching(false);
+              }}
+              placeholder="초대할 팀 메이트의 이메일을 입력해주세요!"
+              style={s.textInput}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              returnKeyType="done"
+            />
+          </View>
+
+          {isSearching ? (
+            /* 검색된 emailContainer */
+            filteredResults.length > 0 && (
+              <View style={s.emailContainer}>
+                <FlatList
+                  data={filteredResults}
+                  keyExtractor={(item) => item.id.toString()}
+                  renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => addEmail(item.id)}>
+                      <Text style={s.emailText}>{item.id}</Text>
+                    </TouchableOpacity>
+                  )}
+                />
+              </View>
+            )
+          ) : (
+            /* 건너뛰기 버튼 */
+            <View>
+              <Pressable>
+                <View style={s.subBtn}>
+                  <Text style={s.subBtnText}>건너뛰기</Text>
+                </View>
+              </Pressable>
+              {addedUserEmailArray.length > 0 && (
+                <View>
+                  <Text style={s.emailAddedTitle}>초대할 이메일</Text>
+                  <View style={s.emailAddedContainer}>
+                    <FlatList
+                      data={addedUserEmailArray}
+                      keyExtractor={(item) => item.id.toString()}
+                      renderItem={({ item }) => (
+                        <View style={s.emailAddedContainer}>
+                          <Text style={s.emailAddedText}>{item.id}</Text>
+
+                          <TouchableOpacity
+                            style={s.xIcon}
+                            onPress={() => removeEmail(item.id)}
+                          >
+                            <Image
+                              source={require("../../images/icons/x.png")}
+                            />
+                          </TouchableOpacity>
+                        </View>
+                      )}
+                    />
+                  </View>
+                </View>
+              )}
+            </View>
+          )}
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -247,5 +288,24 @@ const s = StyleSheet.create({
     padding: 15,
     justifyContent: "center",
     marginLeft: "3%",
+  },
+  emailAddedTitle: {
+    fontSize: 12,
+    color: "#050026",
+    fontWeight: "bold",
+    marginTop: 20,
+    marginLeft: 10,
+  },
+  emailAddedContainer: {
+    flexDirection: "row",
+  },
+  emailAddedText: {
+    fontSize: 14,
+    marginTop: 10,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  xIcon: {
+    alignSelf: "center",
   },
 });
