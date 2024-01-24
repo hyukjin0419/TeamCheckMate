63f3848412d1f9171dfae86583d1b99179ac6979
jeondowon <dowon.9102@gmail.com>
2024-01-24 15:31:34 +0900

    TeamItem FileIcon Style Updated

--- screens/components/Team/TeamItem.js
+++ screens/components/Team/TeamItem.js
@@ -287,7 +287,7 @@ const styles = StyleSheet.create({
   },
   file: {
     width: WINDOW_WIDHT * 0.43,
-    height: WINDOW_HEIGHT * 0.15,
+    height: WINDOW_HEIGHT > 700 ? WINDOW_HEIGHT * 0.15 : WINDOW_HEIGHT * 0.2,
     marginHorizontal: "1%",
     marginTop: "8%",
   },
