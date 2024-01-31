c69a6340a281e08002d5ae4481c32578b0ba7130
Justin <22000456@handong.ac.kr>
2024-01-31 17:11:00 +0900

    Created FrontEnd of PersonalPage.js

--- screens/components/PersonalPage.js
+++ screens/components/PersonalPage.js
@@ -1,12 +1,34 @@
-import { View, Button, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { 
+  View,
+  Button, 
+  StyleSheet, 
+  Text, 
+  TouchableOpacity, 
+  Image,
+  TouchableWithoutFeedback,
+  TextInput,
+ } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import Checkbox from "expo-checkbox"
 import Toast, { BaseToast, ErrorToast } from "react-native-toast-message";
 import { EvilIcons } from "@expo/vector-icons";
 import { showToast, toastConfig } from "../components/Toast";
 import Modal from "react-native-modal"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { auth, db } from "../../firebase";
+import s from "../styles/css.js"
+import PersonalPageBtn from "./PersonalPageFolder/PersonalPageBtn";
+import WeeklyCalendar from "./PersonalPageFolder/WeeklyCalendar";
 
 const PersonalPage = () => {
-  // Visibility of modal
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [isSelected, setSelection] = useState(false);
+  const [isSelected1, setSelection1] = useState(false);
+
+    // Visibility of modal
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   const toggleModal = () => {
@@ -14,10 +36,84 @@ const PersonalPage = () => {
     setIsModalVisible(!isModalVisible);
   }
 
-  return (
-    <View style={styles.container}>
-      <Button title='Open Modal' onPress={toggleModal} />
+  const handlePress = () => {
+    setShowModal(!showModal);
+  };
 
+
+  useEffect(() => {
+    const user = auth.currentUser;
+    setUserEmail(user.email)
+    const fetchUserData = async () => {
+      try {
+        // Reference users collection and userEmail document
+        const userDocRef = doc(db, "user", userEmail);
+  
+        // Fetch document snapshot
+        const docSnap = await getDoc(userDocRef);
+  
+        // If data exists
+        if(docSnap.exists()) {
+          //setUserName to name user chose
+          setUserName(docSnap.data().name)
+        }
+        else {
+          console.log("Document does not exist");
+        }
+      } catch(error){
+        console.log("Error retrieving document");
+      }
+    }
+    fetchUserData();
+  }, [userEmail]);
+
+
+  return(
+    <View style={styles.container}>
+      {/* <View style={{marginTop: 18, flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+        <Text style={{marginTop: 100, marginLeft: 20, fontSize: 20}}>{userName} 님 환영합니다</Text>
+        <PersonalPageBtn />
+      </View> */}
+      <Text style={{marginTop: 100, marginLeft: 20, fontSize: 20}}>{userName} 님 환영합니다</Text>
+      <WeeklyCalendar style={{marginBottom: "10%"}}/>
+      <TouchableOpacity
+        style={styles.categoryClassBtn}
+      >
+        <Text style={styles.addClassBtnText}>팀 등록</Text>
+        <Image
+          source={require("../images/ClassAddBtn.png")}
+          style={{ width: 20, height: 20 }}
+        />
+      </TouchableOpacity>
+      <View style={{flexDirection: "row", marginTop: "2%"}}>
+        <Checkbox
+          value={isSelected}
+          onValueChange={setSelection}
+          style={styles.checkbox}
+          color="#D7D2FF"
+        />
+          <Text style={styles.checkText}>LMS 파일 제출하기</Text>
+        <Text style={{...styles.optionSelect, fontWeight: "bold"}}>. . .</Text>
+      </View>
+      <View style={{flexDirection: "row", marginTop: "5%"}}>
+        <Checkbox
+          value={isSelected1}
+          onValueChange={setSelection1}
+          style={styles.checkbox}
+          color="#D7D2FF"
+        />
+          <Text style={styles.checkText}>그래프 1 완성</Text>
+          <TouchableOpacity onPress={toggleModal} style={styles.optionSelect}>
+            <Text style={{fontWeight: "bold", fontSize: 20}}>. . .</Text>
+          </TouchableOpacity>
+      </View>
+      <PersonalPageBtn />
+      {/* <View>
+        <TextInput 
+          onChangeText={(text) => setEmail(text)}
+          style={s.textInput}
+        />
+      </View> */}
       {/* modal code */}
       <Modal 
         onBackdropPress={() => setIsModalVisible(false)}
@@ -27,7 +123,7 @@ const PersonalPage = () => {
         animationIn="slideInUp"
         animationOut="slideOutDown"
         animationInTiming={200}
-        animationOutTiming={500}  
+        animationOutTiming={200}  
         backdropTransitionInTiming={200} 
         backdropTransitionOutTiming={0}
         style={{justifyContent: "flex-end", margin: 0, flex: 1}}
@@ -38,7 +134,7 @@ const PersonalPage = () => {
             <View style={styles.center}>
               {/* Style for the bar on top of modal */}
               <View style={styles.barIcon} />
-                <Text style={styles.text}>LMS 파인 제출하기</Text>
+                <Text style={styles.text}>그래프 1 완성</Text>
             </View>
             {/* Container for 2 buttons */}
             <View style={{flexDirection: "row", justifyContent: "space-between", marginTop: 50, alignItems: "center"}}>
@@ -62,11 +158,65 @@ export default PersonalPage;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
   },
-  modalContent: {
+  AddBtnContainer: {
+    alignItems: "flex-end",
+    paddingBottom: "2%",
+    paddingHorizontal: "5%",
+  },
+  addOrCloseBtn: {
+    width: 40,
+    height: 40,
+    marginRight: "2%",
+  },
+  modalView: {
+    flex: 1,
+    marginLeft: "15%",
+  },
+  twoBtnContainer: {
+    paddingHorizontal: "6%",
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  addClassBtn: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 24,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 15,
+    marginBottom: 12,
+    marginTop: "2%",
+  },
+  addClassBtnText: {
+    fontFamily: "SUIT-Regular",
+    fontSize: 14,
+    paddingHorizontal: 1,
+    marginRight: 3,
+  },
+  joinClassBtn: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 24,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 15,
+  },
+  categoryClassBtn: {
+    backgroundColor: "#D7D2FF",
+    width: "50%",
+    padding: 10,
+    borderRadius: 24,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 15,
+    marginBottom: 12,
+    marginTop: "2%",
+    marginLeft: "3%",
+    justifyContent: "space-between"
+  },
+    modalContent: {
     backgroundColor: "white",
     paddingTop: 12,
     paddingHorizontal: 12,
@@ -118,5 +268,19 @@ const styles = StyleSheet.create({
     fontSize: 17,
     fontWeight: 400,
     color: "white",
+  },
+  checkbox: {
+    marginLeft: "8%",
+    borderRadius: 6,
+  },
+  checkText: {
+    marginLeft: "3%",
+  },
+  optionSelect: {
+    marginRight: "6%",
+    fontSize: 20,
+    position: "absolute",
+    right: 0,
+    top: -7
   },
 });
