6105d8a4dffcaebaf3e4e7ea3c003bfd9d3e22ff
jeondowon <dowon.9102@gmail.com>
2024-02-13 16:33:54 +0900

    Setting Page Updated

--- screens/components/AppInformationPage.js
+++ screens/components/AppInformationPage.js
@@ -6,11 +6,7 @@ import {
   Image,
   TouchableOpacity,
   Dimensions,
-  Switch,
 } from "react-native";
-import { auth } from "../../firebase";
-import React, { useState, useEffect } from "react";
-import { signOut } from "firebase/auth";
 import s from "../styles/css";
 import { color } from "../styles/colors";
 import { StatusBar } from "expo-status-bar";
@@ -19,86 +15,63 @@ import { useNavigation } from "@react-navigation/core";
 const WINDOW_WIDHT = Dimensions.get("window").width;
 const WINDOW_HEIGHT = Dimensions.get("window").height;
 
-const SettingPage = () => {
+const AppInformationPage = () => {
   const navigation = useNavigation();
 
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   return (
     <View style={s.container}>
       <StatusBar style={"dark"}></StatusBar>
       {/* 뒤로가기 버튼, 팀 등록 헤더와 확인버튼 컨테이너 */}
       <View style={s.headContainer}>
-        <View style={s.headBtn}></View>
-        <Text style={s.title}>설정</Text>
+        <TouchableOpacity
+          style={s.headBtn}
+          onPress={() => {
+            navigation.navigate("SettingPage");
+          }}
+        >
+          <Image
+            style={{
+              width: 8,
+              height: 14,
+            }}
+            source={require("../images/backBtn.png")}
+          />
+        </TouchableOpacity>
+        <Text style={s.title}>앱 정보</Text>
         <View style={s.titleRightBtn}></View>
       </View>
       <View style={styles.container}>
         <View style={styles.optionContainer}>
-          <Text style={styles.text}>알림</Text>
-          <Switch
-            trackColor={{ false: color.deactivated, true: color.activated }}
-            thumbColor={"white"}
-            ios_backgroundColor={color.deactivated}
-            onValueChange={toggleSwitch}
-            value={isEnabled}
-          />
+          <Text style={styles.text}>버전</Text>
+          <Text style={{ marginRight: "2%" }}>Alpha</Text>
         </View>
         <View style={styles.optionContainer}>
-          <Text style={styles.text}>이메일 변경</Text>
+          <Text style={styles.text}>이용약관</Text>
           <Image
             style={{ width: 7, height: 12, marginRight: "2%" }}
             source={require("../images/optionRight.png")}
           />
         </View>
         <View style={styles.optionContainer}>
-          <Text style={styles.text}>비밀번호 변경</Text>
+          <Text style={styles.text}>개인정보 정책</Text>
           <Image
             style={{ width: 7, height: 12, marginRight: "2%" }}
             source={require("../images/optionRight.png")}
           />
         </View>
         <View style={styles.optionContainer}>
-          <Text style={styles.text}>계정 정보 수정</Text>
+          <Text style={styles.text}>오픈소스</Text>
           <Image
             style={{ width: 7, height: 12, marginRight: "2%" }}
             source={require("../images/optionRight.png")}
           />
-        </View>
-        <TouchableOpacity
-          style={styles.optionContainer}
-          onPress={() => navigation.navigate("AppInformationPage")}
-        >
-          <Text style={styles.text}>앱 정보</Text>
-          <Image
-            style={{ width: 7, height: 12, marginRight: "2%" }}
-            source={require("../images/optionRight.png")}
-          />
-        </TouchableOpacity>
-        <View style={styles.optionContainer}>
-          <Text style={styles.text}>문의하기</Text>
-          <Image
-            style={{ width: 7, height: 12, marginRight: "2%" }}
-            source={require("../images/optionRight.png")}
-          />
-        </View>
-        <View style={{ ...s.modalTeamBtnContainer, marginTop: 15 }}>
-          <TouchableOpacity
-            style={styles.logOutBtn}
-            onPress={() => signOut(auth)}
-          >
-            <Text style={styles.logOutText}>로그아웃</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.accountDeleteBtn}>
-            <Text style={styles.accountDeleteText}>탈퇴</Text>
-          </TouchableOpacity>
         </View>
       </View>
     </View>
   );
 };
 
-export default SettingPage;
+export default AppInformationPage;
 
 const styles = StyleSheet.create({
   container: {
