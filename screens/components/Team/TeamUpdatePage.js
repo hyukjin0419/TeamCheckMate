3dbac7c8b004704939869d392a5ee84f031b5d73
jeondowon <dowon.9102@gmail.com>
2024-02-14 16:24:49 +0900

    Color Palette Triangle Position Updated

--- screens/components/Team/TeamUpdatePage.js
+++ screens/components/Team/TeamUpdatePage.js
@@ -439,6 +439,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-end",
     flexDirection: "row",
+    marginRight: "2%",
   },
   confirmBtn: {
     flex: 1,
