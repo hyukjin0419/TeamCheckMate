af1ca5e5c92f6775a1dab38d9e16f1e89e167eae
hyukjin0419 <21900771@handong.ac.kr>
2024-02-15 12:29:13 +0900

    commenting + Haptics

--- screens/components/TeamCheckPage.js
+++ screens/components/TeamCheckPage.js
@@ -35,6 +35,7 @@ const WINDOW_HEIGHT = Dimensions.get("window").height;
 
 export default TeamCheckPage = (props) => {
   const navigation = useNavigation();
+  //AssignmentItem에서 가져온 정보
   const route = useRoute();
   const {
     teamCode,
@@ -47,17 +48,73 @@ export default TeamCheckPage = (props) => {
     dueDate,
   } = route.params;
 
-  //체크리스트를 전부 가지고 있는 객체 배열. 하나의 객체는 하나의 체크를 뜻한다
-  //key값은 writer.
+  //firebase에서 가져온 체크리스트를 저장할 객체배열
   const [checklists, setChecklists] = useState([]);
+  //체크리스트 add시 input을 받을 textinput에 사용될 text
   const [newTaskText, setNewTaskText] = useState("");
+  //체크리스트 edit시 input을 받을 textinput에 사용될 text
   const [editTaskText, setEditTaskText] = useState("");
+  //체크리스트 add시 textinput을 렌더링 하기 위해 boolean값을 담는 객체
   const [isWritingNewTask, setIsWritingNewTask] = useState({});
 
-  //체크리스트 생성을 위한 입력창을 열어주는 함수
+  //Read from Firebase and store in an object array
+  const getCheckLists = async () => {
+    console.log("[TeamcheckPage]: getCheckLists 함수 실행");
+    //체크가 안된 체크리스트가 배열 앞부분에 존재하도록 함수가 작동함.
+    //체크 안된 체크리스트를 담을 배열
+    const uncheckedChecklists = [];
+    //체크 된 체크리스트를 담을 배열
+    const checkedChecklists = [];
+
+    //firebase에서 체크리스트 정보를 가져옴
+    await Promise.all(
+      memberNames.map(async (memberName) => {
+        const querySnapshot = await getDocs(
+          query(
+            collection(
+              db,
+              "team",
+              teamCode,
+              "assignmentList",
+              assignmentId,
+              "memberName",
+              memberName,
+              "checkList"
+            ),
+            //시간별로
+            orderBy("regDate", "asc")
+          )
+        );
+        querySnapshot.forEach((doc) => {
+          const data = { id: doc.id, ...doc.data() };
+          //체크 여부에 따라 다른 배열에 넣고...
+          if (data.isChecked) {
+            checkedChecklists.push(data);
+          } else {
+            uncheckedChecklists.push(data);
+          }
+        });
+      })
+    );
+
+    // 체크된 항목과 체크되지 않은 항목을 합쳐서 최종적인 배열을 생성
+    const combinedChecklists = [...uncheckedChecklists, ...checkedChecklists];
+
+    //프론트에 반영하기 위해 상태 업데이트 함수 사용
+    setChecklists(combinedChecklists);
+  };
+
+  //앱이 처음 렌더링 될때 getCheckList() 실행
+  useEffect(() => {
+    getCheckLists();
+    console.log("[TeamcheckPage]: useEffect[] 실행");
+  }, []);
+
+  //add시 체크리스트 생성을 위한 입력창을 열어주는 함수
   const pressAddBtn = (memberName) => {
     console.log("[TeamcheckPage]: pressAddBtn 함수 실행");
-    // 플러스 버튼을 누를 때 해당 팀 멤버에 대한 입력 창을 열도록 설정
+    //1. 멤버 이름을 parameter로 받는다.
+    //2. 이전 상태(prevIsWritingNewTask)를 받아 해당 상태를 변경한 새로운 객체를 반환한다. 이 과정에서 memberName이라는 키를 사용하여 해당 키의 값을 true로 설정하여 상태를 업데이트한다.
     setIsWritingNewTask((prevIsWritingNewTask) => ({
       ...prevIsWritingNewTask,
       [memberName]: true,
@@ -67,7 +124,14 @@ export default TeamCheckPage = (props) => {
   //Create a new checklist
   const addNewTask = async (memberName, isSubmitedByEnter) => {
     console.log("[TeamcheckPage]: addNewTask 함수 실행");
+    //프론트 먼저 반영 후 firebase 실행 -> 어플의 속도를 높이기 위해
+    //너무 빠른 연속 터치시 문제가 발생할 수 있으나, 사용자가 그런 빠른 터치를 할 상황이 거의 없다고 판단되어 일단 진행.
+
+    //textinput창에 아무것도 입력되지 않았는데 함수가 실행되었다면, 아무것도 추가하지 않고 textinput창을 닫아버리는 (렌더링을 멈춘다) 조건문
     if (newTaskText.trim() !== "") {
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+      //새로운 객체를 생성한다.
+      //이때 이름은 파라미터로, content는 useState를 사용한다
       const newChecklist = {
         writer: memberName,
         isChecked: false,
@@ -76,8 +140,11 @@ export default TeamCheckPage = (props) => {
         modDate: new Date(),
       };
 
+      //프론트에 반영
+      //새로 생성된 체크리스트를 기존에 체크리스트에 이어 붙힌다
       setChecklists((prevChecklists) => [...prevChecklists, newChecklist]);
 
+      //firestorage 참조
       const checkListDoc = addDoc(
         collection(
           db,
@@ -92,12 +159,12 @@ export default TeamCheckPage = (props) => {
         newChecklist
       );
 
+      //만약 사용자가 엔터로 입력했을 시, 다음 항목을 계속 작성할 수 있게 설정하는 조건문
       if (!isSubmitedByEnter) {
-        const updatedIsWritingNewTask = { ...isWritingNewTask };
-        updatedIsWritingNewTask[memberName] = false;
-        setIsWritingNewTask(updatedIsWritingNewTask);
-        console.log(updatedIsWritingNewTask);
+        setIsWritingNewTask((prev) => ({ ...prev, [memberName]: false }));
+        // console.log(updatedIsWritingNewTask);
       }
+      //textinput을 비운다.
       setNewTaskText("");
     } else {
       setIsWritingNewTask((prev) => ({ ...prev, [memberName]: false }));
@@ -105,18 +172,27 @@ export default TeamCheckPage = (props) => {
     await getCheckLists();
   };
 
-  //CheckBox Update
+  //Check 누를 때 상태 업데이트 하는 함수
   const handleCheckboxChange = async (writer, id, newValue) => {
     console.log("[TeamcheckPage]: handleCheckBoxChange함수 실행");
-    // 체크박스 상태 변경
+    // 마찬가지로 프론트 먼저 상태 업데이트 후 백엔드 반영
+    //햅틱 추가
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
 
+    //파라미터로 작성자와, 체크리스트 id(from firebase), 그리고 newValue (checkbox에서 제공)을 받는다
+    //전체 객체 배열인 checklists를 순회하며 작성자와 id가 같으면
+    //찾아낸 객체의 value를 바꾸고 modDate를 업데이트 한 후 updateChecklists에 넣는다
+    //아니면 기존 객체를 updateChecklist에 넣는다
     const updatedChecklists = checklists.map((checklist) =>
       checklist.writer === writer && checklist.id === id
         ? { ...checklist, isChecked: newValue, modDate: new Date() }
         : checklist
     );
 
+    //체크여부에 따라 정렬은 바꾼다.
+    //반환 값이 음수일 경우 a가 앞에 위치
+    //반환 값이 양수일 경우 b가 a보다 앞에 위치
+    //0일경우 변경되지 않음
     updatedChecklists.sort((a, b) => {
       if (a.isChecked === b.isChecked) {
         // 체크 여부가 동일하다면 타임스탬프로 정렬
@@ -130,8 +206,10 @@ export default TeamCheckPage = (props) => {
       }
     });
 
+    //업데이트 된 체크리스트를 프론트에 반영
     setChecklists(updatedChecklists);
 
+    //업데이트 된 체크리스트를 firestorage에 반영 -> 백엔드에서는 체크리스트가 객체배열로 되어있지 않기 때문에, 그냥 해당 문서를 참조해서 값을 바꾼다
     try {
       const docRef = doc(
         db,
@@ -153,83 +231,51 @@ export default TeamCheckPage = (props) => {
     }
   };
 
-  //Read from Firebase and store in an object array
-  const getCheckLists = async () => {
-    console.log("[TeamcheckPage]: getCheckLists 함수 실행");
-    const uncheckedChecklists = [];
-    const checkedChecklists = [];
-
-    await Promise.all(
-      memberNames.map(async (memberName) => {
-        const querySnapshot = await getDocs(
-          query(
-            collection(
-              db,
-              "team",
-              teamCode,
-              "assignmentList",
-              assignmentId,
-              "memberName",
-              memberName,
-              "checkList"
-            ),
-            orderBy("regDate", "asc")
-          )
-        );
-        querySnapshot.forEach((doc) => {
-          const data = { id: doc.id, ...doc.data() };
-          if (data.isChecked) {
-            checkedChecklists.push(data);
-          } else {
-            uncheckedChecklists.push(data);
-          }
-        });
-      })
-    );
-
-    // 체크된 항목과 체크되지 않은 항목을 합쳐서 최종적인 배열을 생성
-    const combinedChecklists = [...uncheckedChecklists, ...checkedChecklists];
-
-    setChecklists(combinedChecklists);
-  };
-
-  useEffect(() => {
-    getCheckLists();
-    console.log("[TeamcheckPage]: useEffect[] 실행");
-  }, []);
-
-  //Update
+  //체크리스트 content 업데이트 버튼을 모달에서 눌렀을따
   const pressEditBtn = () => {
     console.log("[TeamcheckPage]: pressEditBtn 함수 실행");
+    //우선 모달을 닫은 후
     setAssignmentOptionModalVisible(false);
+    //Textinput을 띄워주는 함수를 실행
+    //이때 setTimeout은 모달이 닫는 렌더링과 textinput을 focusing하는 렌더링이 겹치지 않게 분리해주는 역할을 함
     setTimeout(() => {
       readyToUpdateTask(); // 두 번째 함수를 500ms(0.5초) 후에 실행
     }, 400);
   };
 
+  //content를 edit하기 위한 textinput을 열어주는 함수
   const readyToUpdateTask = () => {
     console.log("[TeamcheckPage]: readyToUpdateTask 함수 실행");
-    // setAssignmentOptionModalVisible(false);
 
-    // 체크리스트를 찾습니다.
+    //flatlist안에 checklists배열을 filter과 map을 통해서 렌더링한다.
+    //이때 flatlist는 memberInfo객체 배열 (AssignmentItem에서 route) 가져옴.
+    //checklists는 firebase에서 (getCheckList를 통해) 가져옴.
+    //checklists의 writer과 flatlist item의 이름이 같으면 map을 통해 렌더링
+    //isadditing이 참일때에는 edit을 위한 textinput을 렌더링 하고
+    //그렇지 않다면 해당하는 checklist를 렌더링 해준다
     const foundChecklist = checklists.find(
+      //이때 seletedChecklist는 모달창을 띄울때 설정된다
       (checklist) => checklist.id === selectedChecklist.id
     );
 
-    // 찾은 체크리스트가 있고, isadditing 속성을 true로 설정합니다.
     if (foundChecklist) {
+      //isadditing을 참으로 설정하며, textinput을 열어준다.
       foundChecklist.isadditing = true;
+      //원래 입력되었던 내용을 textinput에 렌더링하기 위한 작업
       setEditTaskText(foundChecklist.content);
-      // 새로운 배열을 생성하여 업데이트합니다.
+      //checklists객체배열을 새로 만들어 업데이트 해주는 작업
       const updatedChecklists = checklists.map((checklist) =>
         checklist.id === foundChecklist.id ? foundChecklist : checklist
       );
+      //프론트에 반영
       setChecklists(updatedChecklists);
     }
   };
 
+  //사용자가 edit을 맞추었을때 실행되는 함수
   const editTask = async () => {
     console.log("[TeamcheckPage]: editTask 함수 실행");
+    //프론트 먼저 반영 후 백엔드 작업
     const foundChecklist = checklists.find(
       (checklist) => checklist.id === selectedChecklist.id
     );
@@ -240,6 +286,7 @@ export default TeamCheckPage = (props) => {
         checklist.id === foundChecklist.id ? foundChecklist : checklist
       );
       setChecklists(updatedChecklists);
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
     }
     try {
       const docRef = doc(
@@ -262,22 +309,25 @@ export default TeamCheckPage = (props) => {
     }
   };
 
+  //delete함수
   const deleteTask = async () => {
     console.log("[TeamcheckPage]: deleteTask 함수 실행");
+    //모달창 닫기
     setAssignmentOptionModalVisible(false);
+    //마찬가지로 프론트 반영 후 백엔드 작업
     try {
-      // 클라이언트 상태에서 선택된 체크리스트를 찾습니다.
       const updatedChecklists = checklists.filter(
         (checklist) => checklist.id !== selectedChecklist.id
       );
       setChecklists(updatedChecklists);
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
       console.log(
         teamCode,
         assignmentId,
         selectedChecklist.writer,
         selectedChecklist.id
       );
-      // 데이터베이스에서 해당 체크리스트를 삭제합니다.
+
       await deleteDoc(
         doc(
           db,
@@ -299,18 +349,15 @@ export default TeamCheckPage = (props) => {
   //모달
   const [assignmentOptionModalVisible, setAssignmentOptionModalVisible] =
     useState(false);
-
+  //모달창 선택시
   const [selectedChecklist, setselectedChecklist] = useState({});
-
   const handleAssignmentOptionPress = (checklist) => {
     console.log("[TeamcheckPage]: handleAssignmentOptionPress 함수 실행");
     setAssignmentOptionModalVisible(!assignmentOptionModalVisible);
     setselectedChecklist(checklist);
   };
-  // console.log(JSON.stringify(checklists, null, 2));
-  return (
-    //헤더 부분
 
+  return (
     <KeyboardAvoidingView
       style={s.container}
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -416,7 +463,6 @@ export default TeamCheckPage = (props) => {
                         onChangeText={(text) => setEditTaskText(text)}
                         onSubmitEditing={() => editTask()}
                         onBlur={() => editTask()}
-                        // blurOnSubmit={false}
                       />
                       <View>
                         <Image
@@ -468,10 +514,7 @@ export default TeamCheckPage = (props) => {
                     autoFocus={true}
                     returnKeyType="done"
                     onChangeText={(text) => setNewTaskText(text)}
-                    onSubmitEditing={() => {
-                      addNewTask(item.name, true),
-                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-                    }}
+                    onSubmitEditing={() => addNewTask(item.name, true)}
                     onBlur={() => addNewTask(item.name, false)}
                     blurOnSubmit={false}
                   />
